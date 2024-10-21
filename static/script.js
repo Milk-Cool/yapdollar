cae35edcb50bbfdebcd375dfb127492455f746b8
Milk-Cool <milan-pl@mail.ru>
2024-10-21 17:11:59 +0300

    XIAO HONG SHUUUU!!!

--- static/script.js
+++ static/script.js
@@ -1,3 +1,5 @@
+const xiaoHongShu = new Audio("https://www.myinstants.com/media/sounds/yapdollar-xiaohongshu.mp3");
+
 const prompt = document.querySelector("#prompt");
 prompt.addEventListener("keydown", async e => {
     if(e.key != "Enter") return;
@@ -7,8 +9,11 @@ prompt.addEventListener("keydown", async e => {
         "method": "POST"
     });
     const t = await f.text();
-    if(f.status == 200)
-        (new Audio(t)).play();
+    if(f.status == 200) {
+        const aud = new Audio(t);
+        aud.onended = () => xiaoHongShu.play();
+        aud.play();
+    }
     else
         alert(t);
     prompt.disabled = false;
